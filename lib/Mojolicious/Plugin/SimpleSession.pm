8568c7062f853fb22c2fe9a074053aeea7fb8e2f
Justin Hawkins <justin@hawkins.id.au>
2010-01-21 11:51:37 +1030

    Use proper temporary dir and path processing.

--- lib/Mojolicious/Plugin/SimpleSession.pm
+++ lib/Mojolicious/Plugin/SimpleSession.pm
@@ -6,6 +6,10 @@ use strict;
 use base 'Mojolicious::Plugin';
 use Digest;
 use Data::Dumper;
+use File::Spec::Functions qw/catfile tmpdir/;
+use Carp qw/croak/;
+
+my $max_time = 60 * 60;    # 1 hour
 
 sub register {
     my ( $self, $app ) = @_;
@@ -31,25 +35,26 @@ sub register {
             }
 
             my $session_data = {};
-            if ( $cookie_hash && -e _hash_filename($cookie_hash) ) {
-                open (my $fh, "<", _hash_filename($cookie_hash)) || die "$!";
+            if ( _cookie_valid($cookie_hash) ) {
+                open( my $fh, "<", _hash_filename($cookie_hash) )
+                  || croak "Could not open file: $!";
                 my $content = '';
-                while (<$fh>) {
-                    $content .= $_;
+                while ( my $line = <$fh> ) {
+                    $content .= $line;
                 }
-                close($fh);
+                close($fh) || croak "Could not close file: $!";
                 eval $content;
-                warn $@ if $@;
+                croak "Could not import session: $@" if $@;
             }
 
             # No cookie was given to us, or there was no file for it,
             # so create it.
             else {
                 my $cookie_hash_value
-                    = time() . rand(1) . $c->tx->remote_address;
+                  = time() . rand(1) . $c->tx->remote_address;
                 my $digester = Digest->new('SHA-1');
                 $digester->add($cookie_hash_value);
-                my $cookie_hash = $digester->hexdigest;
+                $cookie_hash = $digester->hexdigest;
 
                 my $cookie = Mojo::Cookie::Response->new;
                 $cookie->name('session');
@@ -90,23 +95,33 @@ sub register {
     );
 }
 
-
+sub _cookie_valid {
+    my $cookie_hash = shift;
+    return 0 unless ( -e _hash_filename($cookie_hash) );
+    return 0
+      unless (
+        time() - ( stat( _hash_filename($cookie_hash) ) )[9] <= $max_time );
+    return 1;
+}
 
 sub _dump_session {
     my ( $filename, $session_data ) = @_;
-    open my $fh, ">", $filename;
+    my $tmp_filename = $filename . '.tmp';
+    open my $fh, ">", $tmp_filename;
     print $fh Data::Dumper->Dump( [$session_data], ['$session_data'] );
-    close $fh || die "$!";
+    close $fh || croak "Could not close $tmp_filename: $!";
+    rename $tmp_filename,
+      $filename || croak "Could not rename $tmp_filename: $!";
 }
 
 sub _hash_filename {
     my $hash = shift;
-    return "/tmp/$hash.txt";
+    return catfile( tmpdir(), "$hash.txt" );
 }
 
 =head1 NAME
 
-Mojolicious::Plugin::SimpleSession - The great new Mojolicious::Plugin::SimpleSession!
+Mojolicious::Plugin::SimpleSession - Exceedingly Simple Mojolicious Sessions
 
 =head1 VERSION
 
@@ -120,12 +135,15 @@ our $VERSION = '0.01';
 
 Quick summary of what the module does.
 
-Perhaps a little code snippet.
+In the C<handler> subroutine of your Mojolicious application, add:
 
-    use Mojolicious::Plugin::SimpleSession;
+    $self->plugin('simple_session');
 
-    my $foo = Mojolicious::Plugin::SimpleSession->new();
-    ...
+That's it! 
+
+If you need to be able to control expiry, use a database store, or basically
+do anything more intelligent with your sessions, you probably want to look
+at L<Mojolicious::Plugin::Session>.
 
 =head1 AUTHOR
 
@@ -137,17 +155,13 @@ Please report any bugs or feature requests to C<bug-mojolicious-plugin-simpleses
 the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Mojolicious-Plugin-SimpleSession>.  I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
 
-
-
-
 =head1 SUPPORT
 
 You can find documentation for this module with the perldoc command.
 
     perldoc Mojolicious::Plugin::SimpleSession
 
-
-You can also look for information at:
+You can also look for information at: http://hawkins.id.au/notes/perl/modules/mojolicious::plugin::simplesession
 
 =over 4
 
